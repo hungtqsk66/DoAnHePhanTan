99d0de61c877c905bb0b2fd472bb1ea29f56ffc7
gitThinh <111752628+gitThinh@users.noreply.github.com>
2023-07-30 22:34:34 +0700

    Update main.jsx

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,16 +1,17 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Banner from './components/Banner'
-import {BrowserRouter as Router}from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import './index.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Container from './components/Container'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+  <React.StrictMode>
     <Navbar />
     <Banner />
     <Container />
     <Footer />
-  </Router>
+  </React.StrictMode>
 )
