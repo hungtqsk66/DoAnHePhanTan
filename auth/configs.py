12ddbe715f2a24e7944e30ce0f8f54a602da1411
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-08-02 20:31:11 +0700

    Upload các module cần thiết

---
+++ auth/configs.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+
+SECRET_KEY = config("SECRET_KEY")
+ALGORITHM =  config("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
