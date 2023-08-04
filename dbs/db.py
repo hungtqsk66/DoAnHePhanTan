2d86f9c792c709be880e2a6273a6c52bba11c655
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-08-04 20:22:14 +0700

    Update db.py

--- dbs/db.py
+++ dbs/db.py
@@ -1,21 +1,15 @@
 #Singleton pattern
 import pyodbc
-import os
-from dotenv import load_dotenv
+
+from decouple import config
 
 #Class database này dùng để thực hiện truy vấn CSDL SQL Server
 
 class Database:
-    def __init__(self) -> None:
-        load_dotenv()
-        self.conn = pyodbc.connect(
-                    'Driver={'+os.getenv('Driver')+'};'
-                    'Server='+os.getenv('Server')+';'
-                    'Database='+os.getenv('Database')+';'
-                    'Trusted_Connection='+os.getenv('Trusted_Connection')+';'
-                    )
-    #----------------------------------------------------------------#
+    
         
+        
+    #----------------------------------------------------------------#
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Database, cls).__new__(cls)
@@ -23,14 +17,24 @@ class Database:
     
     #----------------------------------------------------------------#
     #Hàm này thực hiện lấy object cursor nếu mình muốn thực hiện các thao tác với nó
-    
-    def getCursor(self):
-        return self.conn.cursor()
+    def load_db_configs(self):
+        conn =  pyodbc.connect(
+                    'Driver={'+config('Driver')+'};'
+                    'Server='+config('Server')+';'
+                    'Database='+config('Database')+';'
+                    'Trusted_Connection='+config('Trusted_Connection')+';'
+                )    
+        
+        return conn
+   
     
     #----------------------------------------------------------------#
     #Hàm này thực hiện truy vấn cơ sở dữ  liệu và return object cursor để thực hiện việc tiếp theo khi cần
     def executeQuery(self, query):
-        cursor = self.getCursor()
-        cursor.execute(query)
-        return cursor
+        conn = self.load_db_configs()
+        cursor = conn.cursor()
+        data = cursor.execute(query).fetchall()
+        conn.commit()
+        conn.close()
+        return data
     
